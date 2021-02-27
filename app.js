8166c94a19fe06d59db7c7fab421a83f1d384ea5
Che Blankenship <cheb@Ches-MacBook-Air.local>
2021-02-27 15:46:42 -0600

    add templates

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('heoouyguy');
+const express   = require("express");
+const app       = express();
+const server    = require('http').createServer(app);
+const path      = require("path");
+const fs        = require("fs");
+const port      = 3000;
+
+
+
+// set static
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/js", express.static(__dirname + "public/js"));
+app.use("/img", express.static(__dirname + "public/img"));
+
+// set the view engine to ejs
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// main page
+app.get('/', function(req, res) {
+    // render the main page
+    res.render("index",{
+        data: "Matta"
+    });
+});
+
+//
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
